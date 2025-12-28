bc71094421250a1a04da66c70b5b17a52d051156
checcocav <62503470+checcocav@users.noreply.github.com>
2025-12-28 22:37:28 +0100

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { supabase } from '@/lib/supabase'
+import { supabase } from '@/lib/supabase/server'
 
 type Guest = {
   first_name: string
