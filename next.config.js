45394d207116e941c99ecbb21f90cc8f769205a0
checcocav <62503470+checcocav@users.noreply.github.com>
2025-12-28 14:05:31 +0000

    Fix next.config.js syntax

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const config = {
-<<<<<<< HEAD
   reactStrictMode: true,
-=======
-  // your config
->>>>>>> 343f97a647e2dc20e684898a15203dd3d2248df1
 };
 
 module.exports = config;
