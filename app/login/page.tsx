c8cf069db5fcacca8f6af6508ecb17967255b600
checcocav <62503470+checcocav@users.noreply.github.com>
2025-12-30 09:40:39 +0100

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,65 +1,31 @@
 'use client'
 
 import { useState } from 'react'
-import { supabase } from '@/lib/supabase'
+import { createClient } from '@/lib/supabase/client'
 
 export default function LoginPage() {
+  const supabase = createClient()
   const [email, setEmail] = useState('')
-  const [sent, setSent] = useState(false)
-  const [loading, setLoading] = useState(false)
 
-  const handleLogin = async () => {
-    if (!email) return
-
-    setLoading(true)
-
-    const { error } = await supabase.auth.signInWithOtp({
+  async function signIn() {
+    await supabase.auth.signInWithOtp({
       email,
       options: {
         emailRedirectTo:
           'https://wedding-villa-bisce-tq5f.vercel.app/auth/callback',
-  },
-})
-
-
-    setLoading(false)
-
-    if (!error) {
-      setSent(true)
-    } else {
-      alert(error.message)
-    }
-  }
-
-  if (sent) {
-    return (
-      <main style={{ padding: 40 }}>
-        <h1>Controlla la tua email 📩</h1>
-        <p>Ti abbiamo inviato un link per accedere.</p>
-      </main>
-    )
+      },
+    })
   }
 
   return (
-    <main style={{ padding: 40 }}>
-      <h1>Accesso invitati</h1>
-      <p>Inserisci la tua email per accedere</p>
-
+    <div>
       <input
         type="email"
-        placeholder="nome@email.com"
+        placeholder="email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
-        style={{ display: 'block', marginTop: 20 }}
       />
-
-      <button
-        onClick={handleLogin}
-        disabled={loading}
-        style={{ marginTop: 20 }}
-      >
-        Accedi
-      </button>
-    </main>
+      <button onClick={signIn}>Accedi</button>
+    </div>
   )
 }
