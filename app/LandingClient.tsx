35d190c1d216d78140ce69f3d65c0d44869a1ac6
checcocav <62503470+checcocav@users.noreply.github.com>
2026-01-29 23:39:44 +0100

    Update LandingClient.tsx

--- app/LandingClient.tsx
+++ app/LandingClient.tsx
@@ -321,7 +321,7 @@ export default function LandingClient() {
         }}>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2835.9!2d11.65!3d44.62!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDTCsDM3JzEyLjAiTiAxMcKwMzknMDAuMCJF!5e0!3m2!1sit!2sit!4v1234567890"
-            width="80%"
+            width="10%"
             height="450"
             style={{ border: 0 }}
             allowFullScreen
